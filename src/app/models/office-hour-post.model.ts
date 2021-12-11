ffa4c2a4efd6522c3a1bda42b102dcf16f264543
Nina Taneja <nat2142@columbia.edu>
2021-12-11 01:22:06 -0500

    Updated Post fields for Add OH

--- src/app/models/office-hour-post.model.ts
+++ src/app/models/office-hour-post.model.ts
@@ -1,13 +1,14 @@
 export class OfficeHourPostModel {
-    ta_uni: string;
+    ta_email: string;
     ta_firstname: string;
     ta_lastname: string;
     location: string;
-    course_id: number;
+    course_number: number;
+    course_name: string;
     zoom_link: string;
     start_time: string;
     end_time: string;
     start_date: string;
     end_date: string;
     oh_days: string;
-  }
+}
